59ce4800b5b92cafb2df0dbd3ed787485070efd5
karasuneo <yusei4520030401@gmail.com>
2024-07-20 17:29:31 +0900

    add : 歩行者の移動開始を通知するサービスを追加

---
+++ server/application/services/start_walking_service.py
@@ -0,0 +1,22 @@
+from domain.repository_impl.trajectory_repository_impl import (
+    RealtimeTrajectoryRepositoryImpl, TrajectoryRepositoryImpl)
+from infrastructure.connection import DBConnection
+
+
+class StartWalkingService:
+    def __init__(
+        self,
+        trajectory_repo: TrajectoryRepositoryImpl,
+        realtime_trajectory_repo: RealtimeTrajectoryRepositoryImpl,
+    ):
+        self.__trajectory_repo = trajectory_repo
+        self.__realtime_trajectory_repo = realtime_trajectory_repo
+
+    def run(self, pedestrian_id: str, floor_map_id: str) -> None:
+        conn = DBConnection.connect()
+
+        trajectory_id = self.__trajectory_repo.save(
+            conn=conn, pedestrian_id=pedestrian_id, floor_map_id=floor_map_id
+        )
+
+        self.__realtime_trajectory_repo.save(conn=conn, trajectory_id=trajectory_id)
