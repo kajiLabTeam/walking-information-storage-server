2230777edea16bec498225b724e209ce804c66ec
karasuneo <yusei4520030401@gmail.com>
2024-08-19 13:22:03 +0900

    add : 一対一対応を強制するSQLを追加

--- docker/postgres/init/3-create_walking_information_model_table copy.sql
+++ docker/postgres/init/3-create_walking_information_model_table copy.sql
@@ -3,25 +3,25 @@
 CREATE TABLE gyroscopes (
     id VARCHAR(26) PRIMARY KEY,
     created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
-    walking_information_id VARCHAR(26) REFERENCES walking_information(id)
+    walking_information_id VARCHAR(26) UNIQUE REFERENCES walking_information(id)
 );
 
 CREATE TABLE accelerometers (
     id VARCHAR(26) PRIMARY KEY,
     created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
-    walking_information_id VARCHAR(26) REFERENCES walking_information(id)
+    walking_information_id VARCHAR(26) UNIQUE REFERENCES walking_information(id)
 );
 
 CREATE TABLE atmospheric_pressures (
     id VARCHAR(26) PRIMARY KEY,
     pressure DECIMAL,
     created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
-    walking_information_id VARCHAR(26) REFERENCES walking_information(id)
+    walking_information_id VARCHAR(26) UNIQUE REFERENCES walking_information(id)
 );
 
 CREATE TABLE ratio_waves (
     id VARCHAR(26) PRIMARY KEY,
     rssi DECIMAL,
     created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
-    walking_information_id VARCHAR(26) REFERENCES walking_information(id)
+    walking_information_id VARCHAR(26) UNIQUE REFERENCES walking_information(id)
 );
