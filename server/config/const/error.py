2cfdc26d6768a117e55038e968b7a4782bbcd732
karasuneo <yusei4520030401@gmail.com>
2024-11-09 15:32:57 +0900

    fix: 安全に乱数生成を行えるよう修正

--- server/config/const/error.py
+++ server/config/const/error.py
@@ -1,22 +1,13 @@
-import random
+import secrets
 
 
 def PARTICLES_STEP_ERROR() -> int:
-    return random.randint(
-        -10,
-        10,
-    )
+    return secrets.choice(range(-10, 11))
 
 
 def PARTICLES_ANGLE_ERROR() -> float:
-    return random.randint(
-        -10,
-        10,
-    )
+    return secrets.choice(range(-10, 11))
 
 
 def PARTICLES_DIRECTION_ERROR() -> float:
-    return random.randint(
-        -90,
-        90,
-    )
+    return secrets.choice(range(-90, 91))
