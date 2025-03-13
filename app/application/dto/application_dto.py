e37136eb8e5f1d4c98bfa5795fb45af793e74f23
karasuneo <yusei4520030401@gmail.com>
2025-03-13 14:34:44 +0900

    refactor: 不要なデータクラスを削除し、コードを簡素化する

--- app/application/dto/application_dto.py
+++ app/application/dto/application_dto.py
@@ -1,25 +1,5 @@
 from dataclasses import dataclass
 
-from app.domain.dataclasses import Pose
-from app.domain.models.walking_parameter.walking_parameter import WalkingParameter
-
-
-@dataclass
-class StartWalkingServiceDto:
-    trajectory_id: str
-    floor_information_id: str
-
-
-@dataclass
-class MovePedestrianServiceDto:
-    pose: Pose
-    walking_parameter: WalkingParameter
-
-
-@dataclass
-class FinishWalkingServiceDto:
-    trajectory_id: str
-
 
 @dataclass
 class GenerateTrajectoryServiceDto:
