2f42e2021f94380d5e5d1752e0f678852e7df6f1
karasuneo <yusei4520030401@gmail.com>
2025-03-13 14:35:41 +0900

    refactor: 不要なDTOを削除し、エクスポートリストを簡素化する

--- app/application/dto/__init__.py
+++ app/application/dto/__init__.py
@@ -1,13 +1,7 @@
 from .application_dto import (
-    FinishWalkingServiceDto,
     GenerateTrajectoryServiceDto,
-    MovePedestrianServiceDto,
-    StartWalkingServiceDto,
 )
 
 __all__ = [
-    "FinishWalkingServiceDto",
     "GenerateTrajectoryServiceDto",
-    "MovePedestrianServiceDto",
-    "StartWalkingServiceDto",
 ]
